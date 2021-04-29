31b58570d63c720df57e0ade09f45875bbc0f1f3
Karma Blackshaw <irenesejah29@gmail.com>
2021-04-29 16:45:56 +0800

    new: test 4

---
+++ four.js
@@ -0,0 +1,22 @@
+/**
+ * Group users into an array of 5
+ *
+ * Expected structure
+ *
+ * [
+ *  [{...}, {...}, {...}, {...}, {...}],
+ *  [{...}, {...}, {...}, {...}, {...}],
+ * ]
+ *
+ * Performance: Execution time must not exceed 0.02 ms
+ */
+
+const perf = require('./utils/perf')
+const users = require('./data/users')
+
+const group = () => {
+  // code here...
+}
+
+perf(group)
+// console.log(group())
